a6da7a912b6b690288d0b1a867aaa0b8cd0921e0
Ehsan <ehsan.faghih72@gmail.com>
2025-11-28 16:17:56 -0500

    Time: 71 ms (5.05%), Space: 17.9 MB (58.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def isValid(self, s):
-        pairs = ["()", "{}", "[]"]
         while "()" in s or "{}" in s or '[]' in s:
-            for p in pairs:
-                s = s.replace(p, "")
+            s = s.replace("()", "").replace('{}', "").replace('[]', "")
         return s == ''
