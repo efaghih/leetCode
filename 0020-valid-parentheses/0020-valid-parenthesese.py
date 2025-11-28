aa78e14f4819693f1d3aa7be3b6d826a3919f0ac
Ehsan <ehsan.faghih72@gmail.com>
2025-11-28 16:21:47 -0500

    Delete 0020-valid-parentheses/0020-valid-parenthesese.py

--- 0020-valid-parentheses/0020-valid-parenthesese.py
+++
@@ -1,5 +0,0 @@
-class Solution(object):
-    def isValid(self, s):
-        while "()" in s or "{}" in s or '[]' in s:
-            s = s.replace("()", "").replace('{}', "").replace('[]', "")
-        return s == ''
