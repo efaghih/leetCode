cc2e9a65b88b4355ab245751379311d18f456b5d
Ehsan <ehsan.faghih72@gmail.com>
2025-11-28 19:53:12 -0500

    Time: 15 ms (15.64%), Space: 19.3 MB (51.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         map = {}
-
+        n = len(nums)
         for i, val in enumerate(nums):
-            map[val] = 1 + map.get(val, 0) 
+            map[val] = 1 + map.get(val, 0)
+            if map[val] > (n/2):
+                return val
         print(map)
 
-        mx = max(map.values())
-        for k, v in map.items():
-            if v == mx:
-                return k
+        
+        
 
