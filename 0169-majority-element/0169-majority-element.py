9423dc2bfd33696a41e8fb9f26503c93b618311f
Ehsan <ehsan.faghih72@gmail.com>
2025-11-28 19:58:50 -0500

    Time: 7 ms (58.46%), Space: 19.4 MB (51.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,9 @@
+import random
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        map = {}
-        n = len(nums)
-        for i, val in enumerate(nums):
-            map[val] = 1 + map.get(val, 0)
-            if map[val] > (n/2):
-                return val
-        print(map)
-
-        
-        
+        majority_count = len(nums) // 2
+        while True:
+            candidate = random.choice(nums)
+            if sum(1 for elem in nums if elem == candidate) > majority_count:
+                return candidate
 
