9a90e9f0a5458aefa599751bc26a6da0948e876a
Ehsan <ehsan.faghih72@gmail.com>
2025-12-23 01:15:13 -0500

    Time: 37 ms (16.47%), Space: 18.4 MB (99.76%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        exst_mp = {}
+        prev = 0
+        dob = 0
+        for i in range(len(nums)):
+            exst_mp[nums[i]] = 1 + exst_mp.get(nums[i], 0)
+        print(dob)
+        print (exst_mp)
+        missed = 0
+        for i in range(1, len(nums)+1):
+            if i not in exst_mp.keys() and missed == 0:
+                missed = i
+            if (i < len(nums)) and exst_mp[nums[i]] == 2:
+                dob = nums[i]
+                
+            
+        
+        return [dob, missed]
