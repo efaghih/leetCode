fbbb9be7ebe8b67c0fe608b4e600b855d93642c9
Ehsan <ehsan.faghih72@gmail.com>
2025-12-23 01:16:34 -0500

    Time: 31 ms (18.17%), Space: 18.8 MB (95.44%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -6,14 +6,12 @@ class Solution:
         for i in range(len(nums)):
             exst_mp[nums[i]] = 1 + exst_mp.get(nums[i], 0)
         print(dob)
-        print (exst_mp)
+        print(exst_mp)
         missed = 0
-        for i in range(1, len(nums)+1):
+        for i in range(1, len(nums) + 1):
             if i not in exst_mp.keys() and missed == 0:
                 missed = i
             if (i < len(nums)) and exst_mp[nums[i]] == 2:
                 dob = nums[i]
-                
-            
-        
+
         return [dob, missed]
