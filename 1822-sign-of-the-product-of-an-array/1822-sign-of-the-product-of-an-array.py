197d8a77bc7a47114221ff0fd7e225241048c15e
Ehsan <ehsan.faghih72@gmail.com>
2026-08-05 22:09:12 -0400

    Time: 4 ms (9.19%), Space: 19.3 MB (71.85%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -8,11 +8,19 @@ class Solution:
             else :
                 return 0
 
-        cnt = 0
+        # cnt = 0
+        # for n in nums:
+        #     if n == 0:
+        #         return 0
+        #     if n < 0:
+        #         cnt += -signFunc(n)
+        # print (cnt)
+        # return 1 if cnt % 2 == 0 else -1
+
+        cnt = 1
         for n in nums:
             if n == 0:
                 return 0
             if n < 0:
-                cnt += -signFunc(n)
-        print (cnt)
-        return 1 if cnt % 2 == 0 else -1
+                cnt *= signFunc(n)
+        return cnt
